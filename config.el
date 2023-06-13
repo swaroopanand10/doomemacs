8d2cf4777f4756cb012bb89f9d8858f104888259
swaroopanand <swaroopanand695@gmail.com>
2023-06-13 16:09:23 +0530

    added lsp doc

--- config.el
+++ config.el
@@ -112,8 +112,10 @@
 
 (define-key evil-normal-state-map (kbd "L") 'next-buffer)
 (define-key evil-normal-state-map (kbd "H") 'previous-buffer)
+(define-key evil-normal-state-map (kbd "K") 'lsp-ui-doc-show)
+(define-key evil-normal-state-map (kbd "J") 'lsp-ui-doc-hide)
 
-;; these keybindings are overriding some default keybindings
+;; these keybindings are overriding some default keybindings and not working
 ;; (define-key evil-normal-state-map (kbd "C-k") 'evil-window-increase-height)
 ;; (define-key evil-normal-state-map (kbd "C-j") 'evil-window-decrease-height)
 ;; (define-key evil-normal-state-map (kbd "C-l") 'evil-window-increase-width)
@@ -121,15 +123,15 @@
 
 
 ;; tree-sitter
-;; (use-package tree-sitter
-;;   :ensure t
-;;   :config
-;;   ;; activate tree-sitter on any buffer containing code for which it has a parser available
-;;   (global-tree-sitter-mode)
-;;   ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
-;;   ;; by switching on and off
-;;   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
-;; (use-package tree-sitter-langs
-;;   :ensure t
-;;   :after tree-sitter)
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
